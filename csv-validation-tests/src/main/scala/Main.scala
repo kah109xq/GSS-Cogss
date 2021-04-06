5e4b0c7948373d2712dac4d1669442a1f3d9a897
kah109xq <niedong082100@yeah.net>
2021-04-06 20:33:47 +0800

    Extract values from rdf graph using jena rdf api

--- csv-validation-tests/src/main/scala/Main.scala
+++ csv-validation-tests/src/main/scala/Main.scala
@@ -2,8 +2,15 @@ import java.io._
 import java.io.File
 import sys.process._
 import java.net.URL
-import scala.language.postfixOps
 import spray.json._
+import scala.language.postfixOps
+import org.apache.jena.rdf.model.{ModelFactory, Property, RDFList}
+import org.apache.jena.riot.{Lang, RDFDataMgr}
+import org.apache.jena.vocabulary.XSD
+import org.apache.jena.rdf.model._
+import org.apache.jena.vocabulary._
+import jdk.internal.loader.Resource
+
 object Main extends App {
 
   def fileDownloader(url: String, filename: String) = {
@@ -20,12 +27,31 @@ object Main extends App {
   val manifestValidationUrl = s"${baseUrl}manifest-validation.jsonld"
   println(manifestValidationUrl)
   pw.write(s"# Auto-generated file based on standard validation CSVW tests from ${baseUrl}manifest-validation.jsonld")
-  pw.close
+
   fileDownloader(manifestValidationUrl, "manifest-validation.jsonld")
-//  Manifest json file read
-  val input_file = "manifest-validation.jsonld"
-  val source = scala.io.Source.fromFile(input_file)
-  val lines = try source.mkString finally source.close()
-  val manifest = lines.parseJson
-  println(manifest)
+
+  private val model = ModelFactory.createDefaultModel
+//  model.read("manifest-validation.jsonld", "JSONLD")
+  model.read("https://w3c.github.io/csvw/tests/manifest-validation.jsonld")
+  val pw2 = new PrintWriter(new File("model.txt" ))
+  model.write(pw2)
+  pw2.close
+  val manifest = model.getResource("https://w3c.github.io/csvw/tests/manifest-validation")
+  val comment = manifest.getProperty(RDFS.comment).getString
+  println(comment)
+
+//  Sample rdf graph read working code in scala
+//  private val modelExample = ModelFactory.createDefaultModel
+//  modelExample.read("vc-db-1.rdf")
+//
+//  val vcard = modelExample.getResource("http://somewhere/JohnSmith")
+//  vcard.addProperty(VCARD.NICKNAME, "Ajay Joseph")
+//  vcard.addProperty(VCARD.NICKNAME, "Adman")
+//  val pw2 = new PrintWriter(new File("modelExample.txt" ))
+//  modelExample.write(pw2)
+//  pw2.close
+//  val fullName: String = vcard.getProperty(VCARD.FN).getString
+//  println(fullName)
+
+  pw.close
 }
