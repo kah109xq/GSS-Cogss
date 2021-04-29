a553045209a46d5563514a6556db23059af951aa
kah109xq <niedong082100@yeah.net>
2021-04-29 12:57:07 +0800

    Implement class row

--- csv-validation-tests/src/main/scala/Cell.scala
+++ csv-validation-tests/src/main/scala/Cell.scala
@@ -1,13 +1,13 @@
 package typeDefinitions
 case class Cell(
-                 aboutUrl: String,
-                 column: Column,
-                 errors: List[String], // revisit! Lists in scala are immutable, if errors need to be updated this will not work
-                 ordered: Boolean,
-                 propertyUrl: String,
-                 row: Row,
-                 stringValue: String,
-                 table: Table,
-                 textDirection: String,
-                 value: String,
-                 valueUrl: String)
+               aboutUrl: String,
+               column: Column,
+               errors: List[String], // revisit! Lists in scala are immutable, if errors need to be updated this will not work
+               ordered: Boolean,
+               propertyUrl: String,
+               row: Row,
+               stringValue: String,
+               table: Table,
+               textDirection: String,
+               value: String,
+               valueUrl: String)
