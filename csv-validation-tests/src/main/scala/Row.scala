69234bf05fd04aed60839f18b30562b8b7f73fad
kah109xq <niedong082100@yeah.net>
2021-05-07 02:37:48 +0800

    Fix class name typo

--- csv-validation-tests/src/main/scala/Row.scala
+++ csv-validation-tests/src/main/scala/Row.scala
@@ -2,7 +2,7 @@ package typeDefinitions
 case class Row(
               cells: List[Cell],
               number: Int, // BigInt ?
-              primaryKey: List[cell], // revisit! (a possibly empty list of cells whose values together provide a unique identifier for this row. This is similar to the name of a column)
+              primaryKey: List[Cell], // revisit! (a possibly empty list of cells whose values together provide a unique identifier for this row. This is similar to the name of a column)
               titles: List[String],
               referencedRows: List[String], // revist! (a possibly empty list of pairs of a foreign key and a row in a table within the same group of tables (which may be another row in the table in which this row appears))
               sourceNumber: Int,
