cf613d41b7b8bcae12634aa39b3df07c74c48d15
kah109xq <niedong082100@yeah.net>
2021-05-19 22:00:48 +0800

    Initial test for DateFormat, check if dates are parsed correctly

---
+++ csv-validation-tests/src/test/scala/DateFormatTest.scala
@@ -0,0 +1,10 @@
+import org.scalatest.FunSuite
+import org.joda.time._
+
+class DateFormatTest extends FunSuite {
+  test("it should parse dates that match yyyy-MM-dd correctly") {
+    val df = DateFormat("yyyy-MM-dd")
+    val date = new LocalDate("2015-03-22")
+    assert(df.parse("2015-03-22")("dateTime") === date)
+  }
+}
