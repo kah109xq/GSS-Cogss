9030e50ab03148ed81b067ff45d62492047fdc36
kah109xq <niedong082100@yeah.net>
2021-05-26 02:03:58 +0800

    Add date pattern regular expressions

--- csv-validation-tests/src/main/scala/DateFormat.scala
+++ csv-validation-tests/src/main/scala/DateFormat.scala
@@ -17,6 +17,22 @@ case class DateFormat(pattern: String, dataType: Option[String] = None) {
     "x" -> "/(?<timezone>[-+]((0[0-9]|1[0-3])([0-5][0-9])?|14(00)?))/".r,
     "xx" -> "/(?<timezone>[-+]((0[0-9]|1[0-3])[0-5][0-9]|1400))/".r,
     "xxx" -> "/(?<timezone>[-+]((0[0-9]|1[0-3]):[0-5][0-9]|14:00))/".r)
+  private var datePatternRegExp = HashMap(
+    "yyyy-MM-dd" -> raw""""^$fields("yyyy")-$fields("MM")-$fields("dd")$$""".r,
+    "yyyyMMdd" -> raw""""^$fields("yyyy")$fields("MM")$fields("dd")$$""".r,
+    "dd-MM-yyyy" -> raw"""^$fields("dd")-$fields("MM")-$fields("yyyy")$$""".r,
+    "d-M-yyyy" -> raw"""^$fields("d")-$fields("M")-$fields("yyyy")$$""".r,
+    "MM-dd-yyyy" -> raw"""^$fields("MM")-$fields("dd")-$fields("yyyy")$$""".r,
+    "M-d-yyyy" -> raw"""^$fields("M")-$fields("d")-$fields("yyyy")$$""".r,
+    "dd/MM/yyyy" -> raw"""^$fields("dd")/$fields("MM")/$fields("yyyy")$$""".r,
+    "d/M/yyyy" -> raw"""^$fields("d")/$fields("M")/$fields("yyyy")$$""".r,
+    "MM/dd/yyyy" -> raw"""^$fields("MM")/$fields("dd")/$fields("yyyy")$$""".r,
+    "M/d/yyyy" -> raw"""^$fields("M")/$fields("d")/$fields("yyyy")$$""".r,
+    "dd.MM.yyyy" -> raw"""^$fields("dd").$fields("MM").$fields("yyyy")$$""".r,
+    "d.M.yyyy" -> raw"""^$fields("d").$fields("M").$fields("yyyy")$$""".r,
+    "MM.dd.yyyy" -> raw"""^$fields("MM").$fields("dd").$fields("yyyy")$$""".r,
+    "M.d.yyyy" -> raw"""^$fields("M").$fields("d").$fields("yyyy")$$""".r
+  )
 
   def parse(value: String): HashMap[String, Any]= {
     throw new NotImplementedError()
