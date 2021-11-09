e567f4c140dac90d729fcbe9b8ca106347bf3ed5
kah109xq <niedong082100@yeah.net>
2021-11-09 12:22:42 +0800

    Set default grouping and decimal separator when not specified

--- src/main/scala/NumberFormat.scala
+++ src/main/scala/NumberFormat.scala
@@ -10,11 +10,15 @@ case class NumberFormat(pattern:Option[String], groupChar: Option[Char] = None, 
   try {
     decimalChar match {
       case Some(c) => decimalFormatSymbols.setDecimalSeparator(c)
-      case _ => {}
+      // If no decimal separator is specified, set it to . (dot)
+      // Setting it since we are not sure of the locale the user running this program will have, and might cause issues
+      // if it is not the default decimal separator .
+      case _ => decimalFormatSymbols.setDecimalSeparator('.')
     }
     groupChar match {
       case Some(c) => decimalFormatSymbols.setGroupingSeparator(c)
-      case _ => {}
+      // If no grouping separator is specified, set it to , (comma)
+      case _ => decimalFormatSymbols.setGroupingSeparator(',')
     }
     df.setDecimalFormatSymbols(decimalFormatSymbols)
     pattern match {
