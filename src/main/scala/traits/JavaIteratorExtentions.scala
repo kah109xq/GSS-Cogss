fbab79d5c903356ee7887ae08caa94bc825776e4
kah109xq <niedong082100@yeah.net>
2021-12-05 13:50:23 +0800

    Traits commented out, fix them

--- src/main/scala/traits/JavaIteratorExtentions.scala
+++ src/main/scala/traits/JavaIteratorExtentions.scala
@@ -1,12 +1,14 @@
-package CSVValidation
-package traits
-import scala.jdk.javaapi.{CollectionConverters => conv}
-import java.{lang => jl, util => ju}
-
-object JavaIteratorExtensions {
-  implicit class IteratorHasAsScalaArray[A](i: ju.Iterator[A]) {
-    /** Converts a Java `Iterator` to a Scala `Array` **/
-
-    def asScalaArray: Array[A] = Array.from(conv.asScala(i))
-  }
-}
+//package CSVValidation
+//package traits
+//
+//import scala.jdk.javaapi.{CollectionConverters => conv}
+//import java.{lang => jl, util => ju}
+//import scala.reflect.ClassTag
+//
+//object JavaIteratorExtensions {
+//  implicit class IteratorHasAsScalaArray[A](i: ju.Iterator[A]) {
+//    /** Converts a Java `Iterator` to a Scala `Array` **/
+//
+//    def asScalaArray: Array[A] = Array.from(conv.asScala(i))
+//  }
+//}
